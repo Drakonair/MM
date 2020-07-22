70770c32b4276541ce1dfc83f36b9a6d405a3234
Drakonair <redface112@gmail.com>
2020-07-22 18:20:15 +0700

    Change SIR.py to Ex2.py, edited Ex2.py, added Ex3.py

---
+++ Ex2.py
@@ -0,0 +1,46 @@
+'''SIR model'''
+
+import matplotlib.pyplot as plt
+import math
+import numpy as np
+from scipy.integrate import odeint
+
+#Declaring differential equations
+def func(y, t, N, beta, gamma):
+    S, I, R = y
+    dSdt = -beta * S * I / N
+    dIdt = beta * S * I / N - gamma * I
+    dRdt = gamma * I
+    return dSdt, dIdt, dRdt
+
+#Solving the equations
+def SIR(t: np.linspace, beta, gamma, S0, I0, R0, N):
+    y0 = S0, I0, R0
+    ret = odeint(func, y0, t, args=(N, beta, gamma))
+    S, I, R = ret.T
+    return S,I,R
+
+#Plotting the diagram
+def plot(t: np.linspace, beta, gamma, S0, I0, R0, N):
+    S, I, R = SIR(t, beta, gamma, S0, I0, R0, N)
+    fig = plt.figure(facecolor='w')
+    g = fig.add_subplot(111, facecolor='#e0fbfc', axisbelow=True)
+    g.plot(t, S/N, 'r', alpha=0.5, lw=2, label='Susceptible')
+    g.plot(t, I/N, 'g', alpha=0.5, lw=2, label='Infected')
+    g.plot(t, R/N, 'b', alpha=0.5, lw=2, label='Recovered')
+    g.set_xlabel('Time unit')
+    g.set_ylabel('Population fraction')
+    g.set_xlim(0,max(t))
+    g.set_ylim(0,max(max(S/N),max(I/N),max(R/N))+0.02)
+    g.yaxis.set_tick_params(length=0)
+    g.xaxis.set_tick_params(length=0)
+    g.grid(b=True, which='major', c='w', lw=2, ls='solid')
+    l = g.legend()
+    legend.get_frame().set_alpha(0.5)
+    for spine in ('top', 'right', 'bottom', 'left'):
+        g.spines[spine].set_visible(False)
+    plt.title("SIR model", size=20)
+    plt.show()
+
+#Example
+plot(np.linspace(0,8,8),2,0.5,800,7,0,1000)
